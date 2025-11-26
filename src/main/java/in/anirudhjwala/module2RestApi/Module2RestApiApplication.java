115c6fe37b47bc91d783af1cb34e9581900d0a1e
anirudhsai2 <anirudhsai2@gmail.com>
2025-11-27 03:48:24 +0530

    Presentation layer, DTO and Controllers

--- src/main/java/in/anirudhjwala/module2RestApi/Module2RestApiApplication.java
+++ src/main/java/in/anirudhjwala/module2RestApi/Module2RestApiApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Module2RestApiApplication {
 
-	public static void main(String[] args) {
+	static void main(String[] args) {
 		SpringApplication.run(Module2RestApiApplication.class, args);
 	}
 
---
+++ src/main/java/in/anirudhjwala/module2RestApi/controllers/EmployeeController.java
@@ -0,0 +1,40 @@
+package in.anirudhjwala.module2RestApi.controllers;
+
+import in.anirudhjwala.module2RestApi.dto.EmployeeDTO;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+
+@RestController
+@RequestMapping("/employees")
+public class EmployeeController {
+
+    @GetMapping(path = "/getSecretMessage")
+    public String getMySuperSecretMessage() {
+        return "Secret Message :: 13AS89$2#239";
+    }
+
+    @GetMapping(path = "/{employeeId}")
+    public EmployeeDTO getEmployeeById(@PathVariable Long employeeId) {
+        return new EmployeeDTO(employeeId, "Anirudh", "hi@anirudhjwala.in", 25, LocalDate.of(2021, 3, 4),true);
+    }
+
+    @GetMapping(path = "/")
+    public String getEmployees(
+            @RequestParam(required = false) Integer age,
+            @RequestParam(required = false) String sortBy
+    ) {
+        return "Employee age :: " + age + " sort by :: " + sortBy;
+    }
+
+    @PostMapping
+    public EmployeeDTO createNewEmployee(@RequestBody EmployeeDTO inputEmployee) {
+        inputEmployee.setId(134090);
+        return inputEmployee;
+    }
+
+    @PutMapping
+    public String updateEmployeeById() {
+        return "Hello from PUT";
+    }
+}
