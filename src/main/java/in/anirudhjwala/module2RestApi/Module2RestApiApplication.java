690794a830a1a2ea296681136e22c031479e4690
anirudhsai2 <anirudhsai2@gmail.com>
2025-11-27 03:02:34 +0530

    spring boot mvc project starter

---
+++ src/main/java/in/anirudhjwala/module2RestApi/Module2RestApiApplication.java
@@ -0,0 +1,13 @@
+package in.anirudhjwala.module2RestApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Module2RestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Module2RestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/in/anirudhjwala/module2RestApi/Module2RestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package in.anirudhjwala.module2RestApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Module2RestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
